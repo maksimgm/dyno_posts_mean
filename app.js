80ed930549c8020eb98dc2061704a22145dabdb7
maksimgm <maksimmamrikov@gmail.com>
2015-12-27 13:50:32 -0800

    add post functionality on front-end

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 var app = angular.module('dynoPosts',[]);
 
 app.controller('MainController',["$scope",function($scope){
-  $scope.test = "Hello World!";
+  $scope.postToggle= false;
+
+  $scope.formToggle = function(){
+    $scope.postToggle = true;
+  };  
 
   $scope.posts = [
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 15},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 5', upvotes: 4}
-];
+    {title: 'post 1', votes: 5},
+    {title: 'post 2', votes: 2},
+    {title: 'post 3', votes: 15},
+    {title: 'post 4', votes: 9},
+    {title: 'post 5', votes: 4}
+  ];
+  $scope.addPost = function(){
+    $scope.postToggle = false;
+  };
+
+  $scope.addPost = function(){
+    $scope.postToggle = false;
+    if(!$scope.data.title || $scope.data.title === '')return;
+    
+    $scope.posts.push({
+      title: $scope.data.title,
+      link: $scope.data.link,
+      votes: 0
+    });
+      $scope.data.link = '';
+      $scope.data.title = '';
+  };
+
+  $scope.incremVotes = function(post){
+    post.votes++;
+  };
+
+  $scope.decremVotes = function(post){
+    post.votes--;
+  };
 }]);
 
